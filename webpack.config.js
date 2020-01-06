c89b2f0ce822dffc248f945a2282fa8b71aef8d2
ioffikus <ioffikus@gmail.com>
2020-01-06 14:35:14 +0300

    updated entry points

--- webpack.config.js
+++ webpack.config.js
@@ -7,49 +7,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: {
     /** ACCOUNTS */
-    'accounts/accounts.service': './src/nodes/accounts/services/accounts/accounts.service.ts',
-    // eslint-disable-next-line
-    'accounts/accounts-history.service': './src/nodes/accounts/services/accountsHistory/accounts-history.service.ts',
-    // eslint-disable-next-line
-    'accounts/activity-timer.service':'./src/nodes/accounts/services/activityTimer/activity-timer.service.ts',
-    'accounts/tokens.service': './src/nodes/accounts/services/tokens/tokens.service.ts',
-
-    /** BILLING */
-    'billing/best2pay.service': './src/nodes/billing/services/best2pay/best2pay.service.ts',
-    'billing/advcash.service': './src/nodes/billing/services/advcash/advcash.service.ts',
-    'billing/currencies.service': './src/nodes/billing/services/currencies/currencies.service.ts',
-    'billing/payments.service': './src/nodes/billing/services/payments/payments.service.ts',
-    'billing/profits.service': './src/nodes/billing/services/profits/profits.service.ts',
-    'billing/wallets.service': './src/nodes/billing/services/wallets/wallets.service.ts',
-    'billing/profits-passports.service':
-      './src/nodes/billing/services/profits-passports/profits-passports.service',
-    'billing/payment-timers.service':
-      './src/nodes/billing/services/payment-timers/payment-timers.service.ts',
-
-    /** E-COMMERCE */
-    // eslint-disable-next-line
-    'ecommerce/categories.service': './src/nodes/ecommerce/services/categories/categories.service.ts',
-    'ecommerce/fields.service': './src/nodes/ecommerce/services/fields/fields.service.ts',
-    'ecommerce/products.service': './src/nodes/ecommerce/services/products/products.service.ts',
-    'ecommerce/suppliers.service': './src/nodes/ecommerce/services/suppliers/suppliers.service.ts',
-    'ecommerce/orders.service': './src/nodes/ecommerce/services/orders/orders.service.ts',
-
-    /** HELPERS **/
-    'helpers/files.service': './src/nodes/helpers/services/files/files.service.ts',
-    'helpers/confirmSMS.service': './src/nodes/helpers/services/confirm-sms/confirm-sms.service.ts',
-
-    /** INTEGRATION */
-    'integration:okmr-crm.service': './src/nodes/integration/okmr-crm/okmr-crm.service.ts',
-    'integration:okmr-im.service': './src/nodes/integration/okmr-im/okmr-im.service.ts',
-    'integration:npow-im.service': './src/nodes/integration/npow-im/npow-im.service.ts',
-
-    /** LOCATIONS */
-    'locations/cities.service': './src/nodes/locations/services/cities/cities.service.ts',
-    'locations/countries.service': './src/nodes/locations/services/countries/countries.service.ts',
-    'locations/regions.service': './src/nodes/locations/services/regions/regions.service.ts',
-
-    /** NOTIFICATIONS */
-    'notifications/sms.service': './src/nodes/notifications/services/sms/sms.service.ts',
+    'accounts/accounts.service': './src/nodes/users/services/accounts/accounts.service.ts',
 
     /** REST */
     // eslint-disable-next-line
